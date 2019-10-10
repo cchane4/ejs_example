b7abcc872802ec96e100f481f9b0badef39ec0a6
Chad Chaney <cchane4@gmail.com>
2019-10-10 17:19:40 -0500

    added ejs

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let express = require('express'); 
+let express = require('express');
 let app = express(); 
 
 
 app.get("/", (req, res) => { 
-    res.send("Welcome to the review page!")
+    res.render("home.ejs"); 
 }); 
 app.listen(3000, () => { 
     console.log("server is listening"); 
