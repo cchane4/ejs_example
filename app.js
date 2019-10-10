aa870a34c49bdcbb3f8e9646d9f93ef912138503
Chad Chaney <cchane4@gmail.com>
2019-10-10 17:47:40 -0500

    using ejs brackets to pass information from app.js to render html on the page

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ let app = express();
 app.get("/", (req, res) => { 
     res.render("home.ejs"); 
 }); 
+
+app.get("/fallinlovewiththing/:thing", (req,res) => { 
+let thing = req.params.thing;
+/*you have to define the variable you want to pass via embedded javascript
+in the res.render line */ 
+res.render('love.ejs', {thingvariable: thing}); 
+}); 
 app.listen(3000, () => { 
     console.log("server is listening"); 
 }); 
