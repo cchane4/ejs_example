fb62aaef15a398332763eaf240a0def40d6be278
Chad Chaney <cchane4@gmail.com>
2019-10-11 13:01:17 -0500

    using a for loop and a for each loop in ejs

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ let thing = req.params.thing;
 in the res.render line */ 
 res.render('love.ejs', {thingvariable: thing}); 
 }); 
+
+app.get("/posts", (req,res) => { 
+    let post_chain = [ 
+        {title: "Where the Red Fern Grows", author: "Suzy"},
+        {title: "Charlotte's Web", author: "Toni"},
+        {title: "The Crucible", author: "Jesse"}
+    ]; 
+    /* res.render posts_a is what we will call it inside the template
+    posts is the array of objects we will loop through */
+    res.render("posts.ejs", {posts_a: post_chain})
+}); 
+
 app.listen(3000, () => { 
     console.log("server is listening"); 
 }); 
